bcc2dc0e3725e8ca124c2886edf77336bc9e1065
Madhu <vellaturu28@gmail.com>
2024-08-08 23:25:18 -0700

    more changes

--- eg5.sh
+++ eg5.sh
@@ -7,3 +7,16 @@ echo
 echo "#### free space####"
 df -h
 echo
+
+echo "### display date ####"
+date
+echo
+
+echo "### uptime ####"
+uptime
+echo
+
+echo "### current file path ####"
+echo $0
+echo
+
